67227f1a6e95f702d9078d52fa37dec214a5c548
sefikspahice <spahicsefik@gmail.com>
2021-10-05 13:49:42 +0200

    done

---
+++ src/components/Popup/index.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from "react";
+import { checkWin } from "../../Helper/index";
+
+const Popup = ({
+  correctLetters,
+  wrongLetters,
+  selectedWord,
+  setPlayable,
+  playAgain,
+}) => {
+  let finalMessage = "";
+  let finalMessageRevealWord = "";
+  let playable = true;
+
+  if (checkWin(correctLetters, wrongLetters, selectedWord) === "win") {
+    finalMessage = "Ma bravo! ";
+    playable = false;
+  } else if (checkWin(correctLetters, wrongLetters, selectedWord) === "lose") {
+    finalMessage = "Drugi put više srece";
+    finalMessageRevealWord = `... da si napisao  "${selectedWord}" pobjedio bi `;
+    playable = false;
+  }
+
+  useEffect(() => {
+    setPlayable(playable);
+  });
+
+  return (
+    <div
+      className="popup-container"
+      style={finalMessage !== "" ? { display: "flex" } : {}}
+    >
+      <div className="popup">
+        <h2>{finalMessage}</h2>
+        <h3>{finalMessageRevealWord}</h3>
+        <button onClick={playAgain}>Play Again</button>
+      </div>
+    </div>
+  );
+};
+
+export default Popup;
---
+++ src/components/WrongLetters/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+const Style = {
+  marginRight: "11.5rem",
+  marginTop: "21rem",
+};
+const WrongLetters = ({ wrongLetters }) => {
+  return (
+    <div className="wrong-letters-container" style={{ color: "#34495e" }}>
+      <div style={Style}>
+        <p>Wrong</p>
+
+        {wrongLetters
+          .map((letter, i) => <span key={i}>{letter}</span>)
+          .reduce(
+            (prev, curr) => (prev === null ? [curr] : [prev, ", ", curr]),
+            null
+          )}
+      </div>
+    </div>
+  );
+};
+
+export default WrongLetters;
